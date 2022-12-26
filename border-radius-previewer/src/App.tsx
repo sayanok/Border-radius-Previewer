a32605365d7851adec3a2e5f476c43777d02a529
bigen1925 <bigen1925@gmail.com>
2022-12-26 15:42:11 +0900

    modify styleを表示させる

--- border-radius-previewer/src/App.tsx
+++ border-radius-previewer/src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App: React.FC = () => {
+  const style = {
+    backgroundColor: "#678F8D",
+    borderRadius: "100px 10px 200px 50px",
+    padding: "20px",
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p style={style}>Rounded corners!</p>
     </div>
   );
-}
+};
 
 export default App;
