e81bd8039e71a99d99719a3ed3c0e2c7b4602579
bigen1925 <bigen1925@gmail.com>
2022-12-28 16:42:03 +0900

    modify
    四隅のborderRadiusを好きに変更できるようにした

--- border-radius-previewer/src/App.tsx
+++ border-radius-previewer/src/App.tsx
@@ -1,15 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const App: React.FC = () => {
-  const style = {
-    backgroundColor: "#678F8D",
-    borderRadius: "100px 10px 200px 50px",
-    padding: "20px",
-  };
+  const [upperLeft, setUpperLeft] = useState<string>("");
+  const [upperRight, setUpperRight] = useState<string>("");
+  const [lowerLeft, setLowerLeft] = useState<string>("");
+  const [lowerRight, setLowerRight] = useState<string>("");
+
+  useEffect(() => {
+    setUpperLeft("10px ");
+    setUpperRight("10px ");
+    setLowerLeft("10px ");
+    setLowerRight("10px ");
+  }, []);
+
+  function onChangeHandler(id: string, value: string) {
+    if (id === "upperLeft") {
+      setUpperLeft(value + "px ");
+    } else if (id === "upperRight") {
+      setUpperRight(value + "px ");
+    } else if (id === "lowerLeft") {
+      setLowerLeft(value + "px ");
+    } else if (id === "lowerRight") {
+      setLowerRight(value + "px ");
+    }
+  }
 
   return (
     <div className="App">
-      <p style={style}>Rounded corners!</p>
+      <form>
+        <label>左上</label>
+        <input id="upperLeft" onChange={(e) => onChangeHandler(e.target.id, e.target.value)}></input>
+        <label>右上</label>
+        <input id="upperRight" onChange={(e) => onChangeHandler(e.target.id, e.target.value)}></input>
+        <label>左下</label>
+        <input id="lowerLeft" onChange={(e) => onChangeHandler(e.target.id, e.target.value)}></input>
+        <label>右下</label>
+        <input id="lowerRight" onChange={(e) => onChangeHandler(e.target.id, e.target.value)}></input>
+      </form>
+      <p
+        style={{
+          borderRadius: upperLeft + upperRight + lowerRight + lowerLeft,
+          backgroundColor: "#678F8D",
+          padding: "100px",
+        }}
+      >
+        Rounded corners!
+      </p>
     </div>
   );
 };
